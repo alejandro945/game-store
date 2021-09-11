e4c26f36771c0340be568bdfb47ce62071d75544
luis486 <luis.murcia1@u.icesi.edu.co>
2021-09-11 17:23:36 -0500

    Implemented game methods

--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -1,96 +1,24 @@
 package controller;
 
 import com.jfoenix.controls.*;
-import javafx.event.*;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
-
-import routes.Route;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.Game;
 
+import routes.Route;
+
 public class AdminController {
-    @FXML
-    private JFXTextField userName;
 
-    @FXML
-    private JFXTextField password;
-
-    @FXML
-    private JFXTextField txtCashier;
-
-    @FXML
-    private TableView<?> tbShelves;
-
-    @FXML
-    private JFXTextField lblCashier;
-
-    @FXML
-    private TableView<?> tbCostumers;
-
-    @FXML
-    private JFXTextField title;
-
-    @FXML
-    private JFXTextField code;
-
-    @FXML
-    private JFXTextField price;
-
-    @FXML
-    private JFXTextField amount;
-
-    @FXML
-    private JFXTextArea review;
+    // -----------------------------------------------ADMIN-DASH-----------------------------
 
     private Stage modal;
-
-    @FXML
-    private TableView<?> tbGames;
-
-    @FXML
-    private TableColumn<?, ?> tblIdGames;
-
-    @FXML
-    private TableColumn<?, ?> tblNameGames;
-
-    @FXML
-    private TableColumn<?, ?> tblAmountGames;
-
-    @FXML
-    private TableColumn<?, ?> tblPriceGames;
-
-    @FXML
-    private TableColumn<?, ?> tblIdShelves;
-
-    @FXML
-    private TableColumn<?, ?> tblGamesShelves;
-
-    @FXML
-    private TableColumn<?, ?> tblAmountShelves;
-
-    @FXML
-    private TableColumn<?, ?> tblIdCostumer;
-
-    @FXML
-    private TableColumn<?, ?> tblGamesCostumers;
-
-    @FXML
-    private TableColumn<?, ?> tblAmountCostumers;
-
-    @FXML
-    public void createGame(ActionEvent event) {
-        if (modal == null) {
-            modal = GameStoreGUI.getInstance().loadModal(Route.GAMEMODAL, this);
-            modal.show();
-        }
-    }
-
-    @FXML
-    public void saveGame(ActionEvent event) {
-
-    }
 
     @FXML
     public void cancelModal(ActionEvent event) {
@@ -108,8 +36,11 @@ public class AdminController {
     }
 
     @FXML
-    public void editCashier(ActionEvent event) {
-
+    public void createGame(ActionEvent event) {
+        if (modal == null) {
+            modal = GameStoreGUI.getInstance().loadModal(Route.GAMEMODAL, this);
+            modal.show();
+        }
     }
 
     @FXML
@@ -117,18 +48,112 @@ public class AdminController {
         GameStoreGUI.getInstance().renderScreen(Route.WELCOME);
     }
 
+    // ---------------------------------------SING-IN------------------------------------------
+    @FXML
+    private JFXTextField userName;
+
+    @FXML
+    private JFXTextField password;
+
     @FXML
     public void signIn(ActionEvent event) {
-        GameStoreGUI.getInstance().renderScreen(Route.ADMDASH);
+        if (userName.getText().equals("root") && password.getText().equals("root")) {
+            GameStoreGUI.getInstance().renderScreen(Route.ADMDASH);
+        } else {
+            GameStoreGUI.getInstance().createAlert("You don´t are admin", Route.ERROR);
+        }
+    }
+
+    // --------------------------------------CREATE-GAMES--------------------------------------------
+
+    @FXML
+    private TableView<Game> tbGames;
+
+    @FXML
+    private TableColumn<?, ?> tblIdGames;
+
+    @FXML
+    private TableColumn<?, ?> tblNameGames;
+
+    @FXML
+    private TableColumn<?, ?> tblAmountGames;
+
+    @FXML
+    private TableColumn<?, ?> tblPriceGames;
+
+    @FXML
+    private TableColumn<?, ?> tblReviewGames;
+
+    @FXML
+    private JFXTextField title;
+
+    @FXML
+    private JFXTextField code;
+
+    @FXML
+    private JFXTextField price;
+
+    @FXML
+    private JFXTextField amount;
+
+    @FXML
+    private JFXTextArea review;
+
+    public Boolean validateFields() {
+        if (price.getText().equals("") || amount.getText().equals("") || review.getText().equals("")
+                || title.getText().equals("")) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    @FXML
+    public void saveGame(ActionEvent event) {
+        if (!validateFields()) {
+            try {
+                GameStoreGUI.getInstance().getGameStore().addGames(title.getText(), review.getText(),
+                        Integer.parseInt(price.getText()), Integer.parseInt(amount.getText()));
+                ;
+            } catch (NumberFormatException e) {
+                GameStoreGUI.getInstance().createAlert("You only can write numbers!", Route.WARNING);
+            }
+        }
+    }
+
+    public void initializeGamesTable() {
+        ObservableList<Game> newGame = FXCollections
+                .observableArrayList(GameStoreGUI.getInstance().getGameStore().getGames());
+
+        tbGames.setItems(newGame);
+        tblIdGames.setCellValueFactory(new PropertyValueFactory<>("code"));
+        tblNameGames.setCellValueFactory(new PropertyValueFactory<>("gameName"));
+        tblReviewGames.setCellValueFactory(new PropertyValueFactory<>("review"));
+        tblAmountGames.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        tblPriceGames.setCellValueFactory(new PropertyValueFactory<>("price"));
+
     }
 
     // ---------------------------------------CREATE-SHELVE----------------------------------------------
 
     @FXML
+    private TableView<?> tbShelves;
+
+    @FXML
+    private TableColumn<?, ?> tblIdShelves;
+
+    @FXML
+    private TableColumn<?, ?> tblGamesShelves;
+
+    @FXML
+    private TableColumn<?, ?> tblAmountShelves;
+
+    @FXML
     private JFXTextField shelveName;
 
     @FXML
-    private JFXComboBox<Game> chooseGame;
+    private JFXComboBox<?> chooseGame;
 
     @FXML
     private JFXChipView<String> txtChip;
@@ -153,4 +178,32 @@ public class AdminController {
         txtChip.getChips().addAll("HOLA", "COMO", "VARELAMK", "SUAREZMK", "MURCIAMK");
 
     }
+
+    // ----------------------------------VIEW-COSTUMERS----------------------------------------
+
+    @FXML
+    private TableView<?> tbCostumers;
+
+    @FXML
+    private TableColumn<?, ?> tblIdCostumer;
+
+    @FXML
+    private TableColumn<?, ?> tblGamesCostumers;
+
+    @FXML
+    private TableColumn<?, ?> tblAmountCostumers;
+
+    // ----------------------------------------CASHIERS----------------------------------------------
+
+    @FXML
+    private JFXTextField txtCashier;
+
+    @FXML
+    private JFXTextField lblCashier;
+
+    @FXML
+    public void editCashier(ActionEvent event) {
+
+    }
+
 }
--- src/model/GameStore.java
+++ src/model/GameStore.java
@@ -13,16 +13,21 @@ public class GameStore {
         games = new ArrayList<>();
     }
 
-    public List<Costumer> getCostumers(){
+    public List<Costumer> getCostumers() {
         return costumers;
     }
 
-    public List<Game> getGames(){
+    public List<Game> getGames() {
         return games;
     }
 
-    public void addClient(String code, String name, ArrayList<Game> games){
+    public void addClient(String code, String name, ArrayList<Game> games) {
         Costumer newClient = new Costumer(code, name, games);
         costumers.add(newClient);
+    }
+
+    public void addGames(String name, String review, int price, int amount) {
+        Game newGame = new Game(games.size() + 1, name, review, price, amount);
+        games.add(newGame);
     }
 }
