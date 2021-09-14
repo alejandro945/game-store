91a361227f51adfe0c1bd79890a1f0238f0f4d05
luis486 <luis.murcia1@u.icesi.edu.co>
2021-09-13 21:07:15 -0500

    minimum changes

--- src/routes/Route.java
+++ src/routes/Route.java
@@ -2,7 +2,7 @@ package routes;
 
 public enum Route {
     MAINPANE("screens/mainPane.fxml"), LOGIN("/ui/screens/admLogin.fxml"), WELCOME("/ui/screens/welcome.fxml"),
-    ICON("/ui/assets/images/Controller.jpg"), SUCCESS("/ui/assets/images/Success.png"),
+    ICON("src/ui/assets/images/Controller.png"), SUCCESS("/ui/assets/images/Success.png"),
     ERROR("/ui/assets/images/Error.png"), WARNING("/ui/assets/images/Warning.png"),
     ALERT("/ui/assets/styles/alert.css"), ECOMMERCE("/ui/screens/ecommerce.fxml"),
     GAMEMODAL("/ui/screens/gameModal.fxml"), ADMDASH("/ui/screens/admDash.fxml"),
