80c6978ab65bc6fb3a91963fea9936d282268b8f
luis486 <luis.murcia1@u.icesi.edu.co>
2021-09-22 17:43:01 -0500

    Implemented some methods in package model and collection test

---
+++ test/collection/QueueTest.java
@@ -0,0 +1,25 @@
+package collection;
+
+import model.Costumer;
+import collection.queue.*;
+
+import org.junit.Test;
+
+public class QueueTest {
+
+    public Queue<Costumer> setUpScenary1() {
+        Queue<Costumer> queue1 = new Queue<>();
+        return queue1;
+    }
+
+    public Queue<Costumer> setUpScenary2() {
+        Queue<Costumer> queue2 = new Queue<>();
+        return queue2;
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
